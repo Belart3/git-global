927482a0d583b8fe660b8117fa6ab4c9b2f13820
Belawu Enoch <enochbelawu8@gmail.com>
2024-10-04 10:41:56 +0100

    edited for test

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,6 @@
 const Logo = () => {
     return ( 
-        <div className="w-16 h-12">
+        <div className="w-16 h-12 size-4">
             <img src="images/logo.png" alt="" className="h-full w-full"/>
         </div>
      );
