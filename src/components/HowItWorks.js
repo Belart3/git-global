10f594ef953437e538c022fe1384f49c0a53881f
Belawu Enoch <enochbelawu8@gmail.com>
2024-10-07 15:34:43 +0100

    fixed how it works section image alignment -monday

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -6,7 +6,7 @@ const HowItWorks = () => {
     return ( 
         <section className="flex flex-col justify-center items-center mx-4 md:mx-8 lg:mx-20 max-w-screen-2xl 2xl:px-0 2xl:mx-auto gap-10 mb-32">
             <SectionHeading title="How Our Immigration Prepare Services Work" src="images/how-it-works.svg"/>
-            <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:place-items-center">
+            <div className="grid grid-cols-1 gap-10 md:grid-cols-2 items-center">
                 <div className="h-96 md:h-500 xl:h-700 w-full max-w-xl xl:max-w-none mx-auto  bg-immigration-section bg-center bg-no-repeat bg-contain"></div>
                 <div className="flex flex-col justify-center items-start max-w-lg xl:max-w-none">
                     {
