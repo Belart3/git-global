d29719dafbc120ad26772ae3e8e7bac7c1625813
Belat <enochbelawu8@gmail.com>
2025-01-10 19:48:13 +0100

    changes

--- api/webhook.js
+++ api/webhook.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
     const data = req.body;
     console.log(`Received a ${req.method} request:`, data);
   
-    res.status(200).json({ message: `Received a ${req.method} request` });
+    res.status(200).json({ message: `Yes Received a ${req.method} request` });
   }
   
   
